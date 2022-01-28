bfbc3e4121b25584079c5361e88aa1fcb2ea6360
Stepan <belapko23@gmail.com>
2022-01-29 02:01:16 +0300

    added users app

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
