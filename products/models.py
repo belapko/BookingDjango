2a615af8210da09104c349faef60a89fd363e49b
Stepan <belapko23@gmail.com>
2022-01-28 03:41:31 +0300

    added models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# Таблицы в БД
+
+class ProductCategory(models.Model):  # Поле id по умолчанию создается внутри класса Model
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True, null=True) # Не обязательно заполнять. Пустое поле по умолчанию
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    image = models.ImageField(upload_to='products_images', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=0, default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
