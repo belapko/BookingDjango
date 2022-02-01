ac563caa4a4bb6730af44aa8f27b22d2f778beeb
Stepan <belapko23@gmail.com>
2022-02-01 22:46:20 +0300

    ajax

--- basket/views.py
+++ basket/views.py
@@ -6,6 +6,9 @@ from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
 
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+
 
 # Create your views here.
 
@@ -35,3 +38,23 @@ def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+#####
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+#####
+
+def basket_edit(request, id, quantity): # basket_id, basket_quantity
+    if is_ajax(request=request):
+        basket = Basket.objects.get(id=id)
+        if quantity > 0 and quantity <= basket.product.quantity :
+            basket.quantity = quantity
+            basket.save()
+        elif quantity == 0:
+            basket.delete()
+    baskets = Basket.objects.filter(user=request.user)
+    context = {
+        'baskets' : baskets
+    }
+    result = render_to_string('basket/basket.html', context) # Рендер страницы с обновлённым контекстом
+    return JsonResponse({'result' : result})
