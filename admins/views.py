0f5a31d1682a387e9012e4ae3253e39de9bf2939
Stepan <belapko23@gmail.com>
2022-02-02 09:44:30 +0300

    update for admins

--- admins/views.py
+++ admins/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 
 from users.models import User
-from admins.forms import UserAdminRegistrationForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 from django.urls import reverse
 
 
@@ -39,9 +39,19 @@ def admin_users(request):
 
 
 # Update
-def admin_users_update(request):
+def admin_users_update(request, id):
+    selected_user =User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)  # instance для отображения полей объекта
     context = {
         'title': 'Обновление пользователя',
+        'form' : form,
+        'selected_user' : selected_user,
     }
     return render(request, 'admins/admin-users-update-delete.html', context)
 
