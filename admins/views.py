0429af9e6ae728e3660fd51c05df9c26aaacafe3
Stepan <belapko23@gmail.com>
2022-02-02 10:02:35 +0300

    added user_passes_test for admins

--- admins/views.py
+++ admins/views.py
@@ -4,8 +4,10 @@ from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 from django.urls import reverse
 
-
+from django.contrib.auth.decorators import user_passes_test
 # Create your views here.
+
+@user_passes_test(lambda u: u.is_staff)
 def index(request):
     context = {
         'title': 'Админ-панель',
@@ -14,6 +16,7 @@ def index(request):
 
 
 # Create
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
@@ -30,6 +33,7 @@ def admin_users_create(request):
 
 
 # Read
+@user_passes_test(lambda u: u.is_staff)
 def admin_users(request):
     context = {
         'title': 'Пользователи',
@@ -39,6 +43,7 @@ def admin_users(request):
 
 
 # Update
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_update(request, id):
     selected_user =User.objects.get(id=id)
     if request.method == 'POST':
@@ -57,6 +62,7 @@ def admin_users_update(request, id):
 
 
 # Delete
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_delete(request, id):
     user = User.objects.get(id=id)
     user.is_active = False
@@ -64,6 +70,7 @@ def admin_users_delete(request, id):
     return HttpResponseRedirect(reverse('admins:admin_users'))
 
 # Activate user
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_activate(request, id):
     user = User.objects.get(id=id)
     user.is_active = True
