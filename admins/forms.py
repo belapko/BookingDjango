651decee1de8cac6efffe385ff20f2905c715b77
Stepan <belapko23@gmail.com>
2022-02-03 16:59:53 +0300

    added CRUD for PRODUCTS in ADMINS

--- admins/forms.py
+++ admins/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from users.forms import UserRegistrationForm, UserProfileForm
 from users.models import User
 
+from products.models import Product, ProductCategory
+from django.forms import ModelForm
+
 
 class UserAdminRegistrationForm(UserRegistrationForm):
     image = forms.ImageField(widget=forms.FileInput(attrs={
@@ -17,3 +20,21 @@ class UserAdminProfileForm(UserProfileForm):
         'class': 'form-control py-4', 'placeholder': 'Имя пользователя', 'readonly': False}))
     email = forms.CharField(widget=forms.EmailInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Адрес электронной почты', 'readonly': False}))
+
+
+class ProductAdminUpdateForm(ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Название'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Описание'}))
+    price = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Стоимость'}))
+    quantity = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Количество'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={
+        'class': 'custom-file-input', 'placeholder': 'Выберите изображение'}), required=False)
+
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'quantity', 'image', 'category']
+
--- admins/views.py
+++ admins/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponseRedirect
 
 from users.models import User
-from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm, ProductAdminUpdateForm
 from django.urls import reverse
 
 from django.contrib.auth.decorators import user_passes_test
+
+from products.models import Product
 # Create your views here.
 
 @user_passes_test(lambda u: u.is_staff)
@@ -76,3 +78,53 @@ def admin_users_activate(request, id):
     user.is_active = True
     user.save()
     return HttpResponseRedirect(reverse('admins:admin_users'))
+
+
+# Read
+def admin_products(request):
+    context = {
+        'title' : 'Админка - Продукты',
+        'products' : Product.objects.all(),
+    }
+    return render(request, 'admins/admin-products-read.html', context)
+
+
+# Update
+def admin_products_update(request, id):
+    selected_product = Product.objects.get(id=id)
+    if request.method == 'POST':
+        form = ProductAdminUpdateForm(instance=selected_product, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_products'))
+    else:
+        form = ProductAdminUpdateForm(instance=selected_product)
+    context = {
+        'title' : 'Обноление продукта',
+        'form' : form,
+        'selected_product' : selected_product,
+    }
+    return render(request, 'admins/admin-products-update-delete.html', context)
+
+
+# Create
+def admin_products_create(request):
+    if request.method == 'POST':
+        form = ProductAdminUpdateForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_products'))
+    else:
+        form = ProductAdminUpdateForm()
+    context = {
+        'title': 'Создание продукта',
+        'form': form,
+    }
+    return render(request, 'admins/admin-products-create.html', context)
+
+
+# Delete
+def admin_products_delete(request, id):
+    product = Product.objects.get(id=id)
+    product.delete()
+    return HttpResponseRedirect(reverse('admins:admin_products'))
